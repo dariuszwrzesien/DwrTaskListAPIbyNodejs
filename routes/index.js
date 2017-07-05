1dd8003511f8b11cf0b5a32b65e2ae113d2b18a8
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-07-05 22:39:55 +0200

    created MVR structure

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+module.exports = app => {
+  app.get("/", (req, res) => {
+    res.json({status: "NTask API"});
+  });
+};
+
+module.exports = app => {
+  const Tasks = app.models.tasks;
+  app.get("/tasks", (req, res) => {
+    Tasks.findAll({}, (tasks) => {
+      res.json({tasks: tasks});
+    })
+  })
+};
