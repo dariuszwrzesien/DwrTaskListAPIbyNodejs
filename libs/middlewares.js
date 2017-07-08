8c6f6e76a65da1e418c35a7e1e29229a88792a89
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-07-08 22:11:46 +0200

    added auth

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,9 +1,10 @@
 import bodyParser from "body-parser";
 
 module.exports = app => {
-  app.set("port", 3000);
-  app.set("json spaces", 4);
+    app.set("port", 3000);
+    app.set("json spaces", 4);
     app.use(bodyParser.json());
+    app.use(app.auth.initialize());
     app.use((req, res, next) => {
         console.log(req.body);
         delete req.body.id;
--- routes/task.js
+++ routes/task.js
@@ -3,14 +3,18 @@ module.exports = app => {
     const Tasks = app.db.models.Tasks;
 
     app.route("/tasks")
+        .all(app.auth.authenticate())
         .get((req, res) => {
-            Tasks.findAll({})
+            Tasks.findAll({
+                where: { user_id: req.user.id }
+            })
                 .then(result => res.json(result))
                 .catch(error => {
                     res.status(412).json({msg: error.message});
                 });
         })
         .post((req, res) => {
+            req.body.user_id = req.user.id;
             Tasks.create(req.body)
                 .then(result => res.json(result))
                 .catch(error => {
@@ -19,31 +23,37 @@ module.exports = app => {
         });
 
     app.route("/tasks/:id")
+        .all(app.auth.authenticate())
         .get((req, res) => {
-            //"/tasks/1": Find a task
-            Tasks.findOne({where: req.params})
-            .then(result => {
-                if (result) {
-                    res.json(result);
-                } else {
-                    res.sendStatus(404);
-                }
-            })
-            .catch(error => {
-                res.status(412).json({msg: error.message});
-            });
+            Tasks.findOne({ where: {
+                id: req.params.id,
+                user_id: req.user.id
+            }})
+                .then(result => {
+                    if (result) {
+                        return res.json(result);
+                    }
+                    return res.sendStatus(404);
+                })
+                .catch(error => {
+                    res.status(412).json({msg: error.message});
+                });
         })
         .put((req, res) => {
-            //"/task/1": Update a task
-            Tasks.update(req.body, {where: req.params})
+            Tasks.update(req.body, { where: {
+                id: req.params.id,
+                user_id: req.user.id
+            }})
                 .then(result => res.sendStatus(204))
                 .catch(error => {
                     res.status(412).json({msg: error.message});
                 });
         })
-        .delete(() => {
-            //"/tasks/1: Delete a task"
-            Tasks.destroy({where: req.params})
+        .delete((req, res) => {
+            Tasks.destroy({ where: {
+                id: req.params.id,
+                user_id: req.user.id
+            }})
                 .then(result => res.sendStatus(204))
                 .catch(error => {
                     res.status(412).json({msg: error.message});
