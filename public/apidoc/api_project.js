6b9302684ca50d3e68dd122d58910160deeeb6a1
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-07-10 22:18:24 +0200

    added API documentation

---
+++ public/apidoc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "dwrtasklistapibynodejs",
+  "version": "1.0.0",
+  "description": "Task list API",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-07-10T20:15:40.708Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
