79d4a1c2c7f17f56b2f52f3360caa5760dba3b1b
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-07-11 21:35:59 +0200

    run via cluster

---
+++ clusters.js
@@ -0,0 +1,21 @@
+import cluster from "clusters";
+import os from "os";
+
+const CPUS = os.cpus();
+
+if (cluster.isMaster) {
+    CPUS.forEach(() => cluster.fork());
+    cluster.on("listening", worker => {
+            console.log("Cluster %d connected", worker.process.pid);
+        });
+    cluster.on("disconnect", worker => {
+            console.log("Cluster %d disconnected", worker.process.pid);
+        });
+    cluster.on("exit", worker => {
+    console.log("Cluster %d is dead", worker.process.pid);
+    cluster.fork();
+    // Ensure starts of a new cluster if an old one dies
+    });
+} else {
+    require("./index.js");
+}
