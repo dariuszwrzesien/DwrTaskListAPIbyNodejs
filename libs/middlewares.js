a169eb383b7bd91fbed2cc7c4b4c7d5a80d95198
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2017-07-11 22:03:00 +0200

    added protection to API

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -2,6 +2,8 @@ import bodyParser from "body-parser";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import compression from "compression";
+import helmet from "helmet";
 import logger from "./logger.js";
 
 module.exports = app => {
@@ -13,12 +15,14 @@ module.exports = app => {
                 logger.info(message);
             }
         }
-    }))
+    }));
+    app.use(helmet());
     app.use(cors({
         origin: ["http://localhost:3001"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"]
     }));
+    app.use(compression());
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
     app.use((req, res, next) => {
